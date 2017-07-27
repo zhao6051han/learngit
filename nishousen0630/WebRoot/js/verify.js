481f407e82fd8dbacb1c01d39738db16253cb345
zhaohan <694676765@qq.com>
2017-07-27 16:08:41 +0800

    add

--- nishousen0630/WebRoot/js/verify.js
+++
@@ -1,8 +0,0 @@
-function verify() {
-	var userName = $("#userName").val();
-	//alert("123");
-	$.get("checkUser.action",{"userName":userName},callback);
-}
-function callback(data) {
-		$("#check").html(data);
-}
